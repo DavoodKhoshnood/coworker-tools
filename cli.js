2b820b9307eb98ef152e1b31817e7006665aacdf
Davood Khoshnood <45537769+DavoodKhoshnood@users.noreply.github.com>
2022-02-11 20:18:10 +0000

    List of all members with change

--- cli.js
+++ cli.js
@@ -23,7 +23,7 @@ if (command === 'list') {
 function runListCommand() {
   // Replace this with your implementation
   members.forEach((member) =>
-    console.log(`${member.firstName} ${member.lastName}`),
+    console.log(`Member name : ${member.firstName} ${member.lastName}`),
   )
   console.log('Called the list command')
 }
